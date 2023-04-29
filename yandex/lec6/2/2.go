ec0fa2472e2c7326254b7e9dc1b84164cf84fc84
Prokopov <prokopov.vrn@gmail.com>
2023-04-29 21:39:50 +0300

    lec 6 task2(fix)

--- yandex/lec6/2/2.go
+++ yandex/lec6/2/2.go
@@ -1,39 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	first := []int{1, 3, 5, 8, 9, 9}
-	second := []int{10, 2, 4, 8, 1, 6}
+	sc := bufio.NewScanner(os.Stdin)
+	toInt(sc)
+
+	first := toInt(sc)
+	second := toInt(sc)
 
 	for _, item := range second {
-		ans := lBinSearch(first, item)
-		if ans != len(first) && (ans == 0 || (Abs(first[ans]-item) < Abs(first[ans-1]-item))) {
-			fmt.Println(first[ans])
-		} else {
-			fmt.Println(first[ans-1])
-		}
+		fmt.Println(findClosest(first, item))
 	}
+
 }
 
-func lBinSearch(array []int, value int) int {
+func toInt(scanner *bufio.Scanner) []int {
+	scanner.Scan()
+	array := strings.Split(scanner.Text(), " ")
+	var res []int
+	for _, item := range array {
+		intItem, _ := strconv.Atoi(item)
+		res = append(res, intItem)
+	}
+	return res
+}
+
+func lBinSearch(arr []int, value int) int {
 	l := 0
-	r := len(array)
+	r := len(arr) - 1
 	for l < r {
 		m := (l + r) / 2
-		if array[m] > value {
+		if arr[m] >= value {
 			r = m
-		} else if array[m] < value {
-			l = m + 1
 		} else {
-			return m
+			l = m + 1
 		}
 	}
 	return l
 }
-func Abs(x int) int {
-	if x > 0 {
-		return x
+
+func rBinSearch(arr []int, value int) int {
+	l := 0
+	r := len(arr) - 1
+	for l < r {
+		mid := (l + r + 1) / 2
+		if arr[mid] <= value {
+			l = mid
+		} else {
+			r = mid - 1
+		}
 	}
-	return -x
+	return l
+}
+
+func findClosest(arr []int, value int) int {
+	lSearch := lBinSearch(arr, value)
+	rSearch := rBinSearch(arr, value)
+	if abs(arr[rSearch]-value) > abs(arr[lSearch]-value) {
+		return arr[lSearch]
+	}
+	return arr[rSearch]
+
+}
+
+func abs(i int) int {
+	if i >= 0 {
+		return i
+	}
+	return -i
 }
